06b207aa5124a46713f4767e2cb29244cb698bd1
Julien <julien.marchand4@gmail.com>
2014-09-25 16:17:12 +0200

    Action first structure

---
+++ src/Action.java
@@ -0,0 +1,20 @@
+
+public class Action {
+	private String name;
+	
+	private double[] data;
+	private double[] open;
+	private double[] high;
+	private double[] low;
+	private double[] close;
+	private double[] volume;
+	private double[] adjClose;
+	
+	public void getData() {
+		
+	}
+	
+	public void prepareData() {
+		
+	}
+}
